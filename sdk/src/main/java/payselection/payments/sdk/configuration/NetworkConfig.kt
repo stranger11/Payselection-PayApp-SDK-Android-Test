33bf307f28e2772373a48043ec07818e0483f0d9
Mamatsiuk Aliaksandr <aliaksandr.m@gofarsoft.org>
2023-06-19 16:55:51 +0300

    Changed default host url to  "https://pgw.payselection.com"

--- sdk/src/main/java/payselection/payments/sdk/configuration/NetworkConfig.kt
+++ sdk/src/main/java/payselection/payments/sdk/configuration/NetworkConfig.kt
@@ -3,7 +3,7 @@ package payselection.payments.sdk.configuration
 import java.util.concurrent.TimeUnit
 
 class NetworkConfig(
-    val serverUrl: String = "https://gw.payselection.com",
+    val serverUrl: String = "https://pgw.payselection.com",
     val connectionTimeOut: Long = TimeUnit.SECONDS.toMillis(90),
     val readWriteTimeOut: Long = TimeUnit.SECONDS.toMillis(90)
 )
